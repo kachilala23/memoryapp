8ae1852c5f5b8a08b32b38fea6176b1ffe95a4fc
kachilala <kachilala23@gmail.com>
2021-02-17 12:15:19 +0100

    complete get and post request end point

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,11 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
+
+app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
@@ -17,3 +21,4 @@ mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: tr
   .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set('useFindAndModify', false);
+
---
+++ backend/routes/posts.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+import { getPosts, createPost } from '../controllers/posts.js';
+
+const router = express.Router();
+
+
+router.get('/', getPosts);
+router.post('/', createPost);
+// router.put('/:id', updatePost);
+// router.delete('/:id', deletePost);
+
+export default router;
